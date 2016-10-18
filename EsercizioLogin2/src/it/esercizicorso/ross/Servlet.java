7e1d1a8e633cd49c9315190dac1ca212efebec7c
Rossella <corso@10.0.1.196>
2016-10-18 11:32:27 +0200

    es2 funzionante

---
+++ EsercizioLogin2/src/it/esercizicorso/ross/Servlet.java
@@ -0,0 +1,52 @@
+package it.esercizicorso.ross;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+				String nome = request.getParameter("username");
+				String password = request.getParameter("password");
+				
+				
+				if(password.equals("123")){
+					RequestDispatcher rd = request.getRequestDispatcher("ServletBenvNome");
+					rd.forward(request, response);
+				
+				}else {
+					response.sendRedirect("login.html");
+	}
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
